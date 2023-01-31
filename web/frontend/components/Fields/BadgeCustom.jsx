e8bb41e3b6230c0609ad5ab2002b91f5398d2c9e
ankit.chavhan <ankit.chavhan@techinfini.com>
2023-01-31 10:01:52 +0530

    merging from sj_dev

--- web/frontend/components/Fields/BadgeCustom.jsx
+++
@@ -1,7 +0,0 @@
-import { Badge } from '@shopify/polaris';
-import React from 'react';
-
-function BadgeCustom(props) {
-    return <Badge status={props.status}>{props.text}</Badge>;
-}
-export default BadgeCustom
